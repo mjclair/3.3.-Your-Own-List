6ae9a511d5b633bcc91a59f1d73f8648d28d18ca
Michael Clair <michaelclair@students.abtech.edu>
2025-01-25 11:55:16 -0500

    Add files via upload

---
+++ 3.3. My Own List.py
@@ -0,0 +1,8 @@
+# List of transportation examples
+transportation = ['electric scooter', 'train', 'limo', 'airplane']
+
+# Print statements about each item in the list
+print("I would like to ride around my neighborhood in an " + transportation[0] + ".")
+print("I would travel across the country in a " + transportation[1] + ".")
+print("I would like to ride in a " + transportation[2] + ".")
+print("I enjoy flying in an " + transportation[3] + ".")
